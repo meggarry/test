1df695fdd706aee56888126bf575ac8988f3bc3e
meggarry <megannegarry@gmail.com>
2024-05-28 18:03:53 -0400

    test

---
+++ Lists.py
@@ -0,0 +1,20 @@
+# fruits = ['apple', ' orange', 'banana', 'kiwi', 'mango']
+# # print(fruits)
+#
+# # print(fruits[4])
+#
+# numbers = [23, 45, 12, 67, 89, 100, 2, 5, -5]
+#
+# # print(numbers[4]+numbers[5])
+# # print(numbers[0]*numbers[7])
+#
+num=[27, 53, 33, 47, 124, 73, 9, 8, 3, 13, 56, 86, 97]
+#
+#print(num[0]*num[8]+num[11])
+print(num[3:5]) #just 3 and 5
+print(num[3:]) #starts at 3
+print(num[0:99])
+print(num[-2])
+print(num[::1])
+print(num[::-1]) #reverse order
+print(num[3::5])
