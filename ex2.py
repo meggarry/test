0386a66e8bcc3276e8f6c2bcc5bc1734eec9f16d
meggarry <megannegarry@gmail.com>
2024-05-28 18:47:54 -0400

    test

---
+++ ex2.py
@@ -0,0 +1,27 @@
+# even=[2,4,6,8,10,12,14,16,18,20]
+# print(even)
+# even.append(22)
+# print(even)
+# even.insert(10,21)
+# print(even)
+# print(even[::-1])
+# del(even[6])
+# print(even)
+# lenth=len(even)
+# print(lenth)
+# even.clear()
+# print(even)
+
+
+str1= "This is American University.  It is a great place to study"
+print(str1[3:9])
+
+list1=str1.split()
+print(list1)
+list1=str1.split(".")
+print(list1)
+list2=str1.split("rea")
+print(list2)
+str2=" ".join(list1)
+print(str2)
+
---
+++ matrices.py
@@ -0,0 +1,4 @@
+matrix1= [ [2, 4, 16], [3, 9, 81], [4, 16, 64,[1,2,3]]]
+print(matrix1[1][1])#identifies list 1(second list) and index 1 (second number)
+print(matrix1[2][2])
+print(matrix1[2][3])
